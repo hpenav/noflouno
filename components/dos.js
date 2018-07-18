fb153e2960e5083f368700f75ee8c5c9994c05ad
hpenav <domotica.humberto@gmail.com>
2018-07-18 13:14:26 -0500

    a

---
+++ components/dos.js
@@ -0,0 +1,31 @@
+var noflo = require('noflo');
+
+exports.getComponent = function() {
+  var c = new noflo.Component();
+  c.description = 'dos';
+  c.icon = 'cog';
+  c.inPorts.add('in', {
+    datatype: 'all',
+    description: 'Packet to forward'
+  });
+  c.outPorts.add('out', {
+    datatype: 'all'
+  });
+  c.process(function (input, output) {
+    // Check preconditions on input data
+    if (!input.hasData('in')) {
+      return;
+    }
+    // Read packets we need to process
+    
+    var data = input.getData('in');
+    console.log("-->>: " + data);
+    // Process data and send output
+    output.send({
+      out: data
+    });
+    // Deactivate
+    output.done();
+  });
+  return c;
+};
