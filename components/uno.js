7d2e621757167f70402a61f780228897bc48d063
hpenav <domotica.humberto@gmail.com>
2018-07-18 13:02:52 -0500

    a

---
+++ components/uno.js
@@ -0,0 +1,30 @@
+var noflo = require('noflo');
+
+exports.getComponent = function() {
+  var c = new noflo.Component();
+  c.description = 'uno';
+  c.icon = 'adjust';
+  c.inPorts.add('in', {
+    datatype: 'all',
+    description: 'Packet to forward'
+  });
+  c.outPorts.add('out', {
+    datatype: 'all'
+  });
+  c.process(function (input, output) {
+    // Check preconditions on input data
+    if (!input.hasData('in')) {
+      return;
+    }
+    console.log("heyy...");
+    // Read packets we need to process
+    var data = input.getData('in');
+    // Process data and send output
+    output.send({
+      out: data
+    });
+    // Deactivate
+    output.done();
+  });
+  return c;
+};
